668d6804d5cb86b1435cebd0965068148b81150f
Gustavo Bessa <gbvirtual@gmail.com>
2018-10-02 14:51:15 -0300

    Annotations

---
+++ src/main/java/br/com/hoptech/core/Annotations.java
@@ -0,0 +1,18 @@
+package br.com.hoptech.core;
+
+import java.lang.annotation.Target;
+
+public class Annotations {
+    
+//    @Target(ElementType.METHOD)
+//    @Retention(RetentionPolicy.RUNTIME)
+//    @interface FamilyBudget {
+//       String userRole() default "GUEST";
+//    }
+    
+//       Here, we define an annotation , where  is the only member in that custom annotation. The takes only  type values, 
+//    and the default is "GUEST". If we do not define the value for this annotation member, then it takes the default. 
+//    By using @Target, we can specify where our annotation can be used. For example, the annotation can only be used with the method 
+//    in a class. @Retention defines whether the annotation is available at runtime. 
+
+}
