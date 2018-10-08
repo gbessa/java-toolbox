9afec44ff5571f21e85ff923e9cfa663ba4827a4
Gustavo Bessa <gbvirtual@gmail.com>
2018-10-08 08:58:17 -0300

    Regex

---
+++ src/main/java/br/com/hoptech/regex/HtmlHandle.java
@@ -0,0 +1,31 @@
+package br.com.hoptech.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HtmlHandle {
+
+    public static void main(String[] args) {
+
+	String line = "<h1>teste 123</h1>";
+
+	boolean matchFound = false;
+	Pattern r = Pattern.compile("<(.+)>([^<]+)</\\1>");
+	// <(.+)>  Pega tag de início com qualquer conteúdo
+	// ([^<]+) Pega qualquer conjunto de caracteres, exceto <
+	// </\\1>  Pega tag de fechamento, onde o tipo da TAB dê match com a tag de abertura
+	
+	Matcher m = r.matcher(line);
+
+	while (m.find()) {
+	    System.out.println(m.group(2));
+	    matchFound = true;
+	}
+	
+	if ( ! matchFound) {
+	    System.out.println("None");
+	}
+
+    }
+
+}
