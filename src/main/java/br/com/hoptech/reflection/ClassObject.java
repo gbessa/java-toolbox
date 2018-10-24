dd5f496e84cc23936188e45c5381532b1eac9f2a
Gustavo Bessa <gbvirtual@gmail.com>
2018-10-23 21:00:22 -0300

    Reflection in java 9

--- src/main/java/br/com/hoptech/reflection/ClassObject.java
+++ src/main/java/br/com/hoptech/reflection/ClassObject.java
@@ -1,31 +1,43 @@
 package br.com.hoptech.reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Formas de instanciar o Class Object
  */
 public class ClassObject {
 
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+	//Basicamente pelo insegurança devido ao newInstance não nos avisar se a classe recebe no contrutor objetos de classes que possuem Checked Exceptions
 
-	//Pelo .class da classe
+	//->-> Pelo .class da classe
 	Class<ExemploController> classCtrl1 = ExemploController.class;
-	ExemploController objCtrl1 = classCtrl1.newInstance();
+	ExemploController objCtrl1 = classCtrl1.newInstance(); //Deprecated
 	
-	//Pelo .getClass
+	Constructor<ExemploController> constructor = classCtrl1.getConstructor();
+	ExemploController objCtrl11 = constructor.newInstance();
+	//obs: 
+	//se o contrutor fosse privado, teria que pegar ele pelo getDeclaredContructor(), e deixá-lo acessível pelo .setAcessible(true)
+	
+	//->-> Pelo .getClass
 	ExemploController exemploControler = new ExemploController();
 	Class<? extends ExemploController> classCtrl2 = exemploControler.getClass();
-	ExemploController objCtrl2 = classCtrl2.newInstance();
+	ExemploController objCtrl2 = classCtrl2.newInstance(); //Deprecated
 	
-	//Pelo .formName passando o Fully Qualified Name
+	//->-> Pelo .formName passando o Fully Qualified Name
 	Class<?> classCtrl3 = Class.forName("br.com.hoptech.reflection.ExemploController");
-	Object objCtrl3 = classCtrl3.newInstance();
+	Object objCtrl3 = classCtrl3.newInstance(); //Deprecated
 	
 	//Verifica-se que todos os objetos são instâncias de ExemploController
 	System.out.println(objCtrl1 instanceof ExemploController);
+	System.out.println(objCtrl11 instanceof ExemploController);
 	System.out.println(objCtrl2 instanceof ExemploController);
 	System.out.println(objCtrl3 instanceof ExemploController);
 	
 	//A terceira forma é a mais flexível, e é a que frameworks MVC (como vraptor/spring) usam
+	
+	//O newInstance foi depreciado a partir da versão 9
 	
     }
 
