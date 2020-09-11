ad32168cb5bda3ae968ed267dc3efba923a53675
Gustavo Bessa <gbvirtual@gmail.com>
2020-09-11 10:50:08 +0100

    Adapter DP

---
+++ src/main/java/br/com/hoptech/designpatterns/Adapter.java
@@ -0,0 +1,52 @@
+package br.com.hoptech.designpatterns;
+
+public class Adapter {
+}
+
+class ClasseA {
+
+    public static void main(String[] args) {
+        //Before
+        ClasseB cbOld = new ClasseB();
+        cbOld.print("Hi old world!");
+        //Can use ClasseB, but not ClassC without modifications
+
+        //After
+        IB cb = new ClasseB();
+        cb.print("Hi world !!!!!");
+        IB cc = new AdapterCToB(new ClasseC());
+        cc.print("Hi adapted world !!!!!");
+
+    }
+
+}
+
+class ClasseB implements IB{
+
+    @Override
+    public void print(String s) {
+        System.out.println(s);
+    }
+}
+
+class ClasseC {
+    public void imprimir(String s) {
+        System.out.println(s);
+    }
+}
+
+class AdapterCToB implements IB {
+    private ClasseC classeC;
+
+    public AdapterCToB(ClasseC classeC) {
+        this.classeC = classeC;
+    }
+    @Override
+    public void print(String s) {
+        this.classeC.imprimir(s);
+    }
+}
+
+interface IB {
+    public void print(String s);
+}
---
+++ src/test/java/br/com/hoptech/designpatterns/AdapterTest.java
@@ -0,0 +1,7 @@
+package br.com.hoptech.designpatterns;
+
+public class AdapterTest {
+
+}
+
+
