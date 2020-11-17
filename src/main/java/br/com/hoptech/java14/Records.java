9a66d4c12237dd09c8f47d1835c37d636996f7d5
Gustavo Bessa <gbvirtual@gmail.com>
2020-11-17 11:28:49 +0000

    Records

---
+++ src/main/java/br/com/hoptech/java14/Records.java
@@ -0,0 +1,33 @@
+package br.com.hoptech.java14;
+/*
+Similar to case classes in scala and sealed classes in kotlin
+They are Data Classes
+Mean to be:
+immutable (ensures the data without synchronization
+comparable (no need to override the hash/equals)
+Adapted to functional programming
+They implement:
+1. final private fields
+2. getters
+3. public constructor with parameters
+4. equals
+5. hashCode
+6. toString
+BUT, the version 14 is not LTS. The last LTS is the 11.
+ */
+public class Records {
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Gustavo", 1.78);
+        Person person2 = new Person("Gustavo", 1.78);
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1);
+
+    }
+}
+
+record Person (String name, double height) {}
+
+
+
